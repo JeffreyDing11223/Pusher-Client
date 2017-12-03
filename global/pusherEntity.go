60a34b9e652addf3d0feb59fdda02e7aa623ed52
djDing <jeffreyding@foxmail.com>
2017-12-03 13:19:13 +0800

    Update pusherEntity.go

--- global/pusherEntity.go
+++ global/pusherEntity.go
@@ -3,4 +3,4 @@ package global
 
 import "github.com/JeffreyDing11223/Pusher-Client/pusher"
 
-var PusherCli *pusher.PusherClient
+var PusherCli *pusher.PusherClient   //define your own pusher entity
